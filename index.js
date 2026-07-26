35b7b4833bffbc988e8e21c0765b1054058c3b30
Tactor <119103845+tactordev@users.noreply.github.com>
2026-07-26 20:04:30 +0100

    Command handling

---
+++ index.js
@@ -0,0 +1,130 @@
+require("dotenv").config();
+
+
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+
+// discord bot token
+const TOKEN = process.env.TOKEN;
+
+
+
+// client setup
+const client = new Client(
+    {
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers
+        ]
+    }
+);
+
+
+// collections to add cmds to
+client.prefixCmds = new Collection();
+client.slashCmds = new Collection();
+client.subcommandHandlers = new Collection();
+
+
+
+// fetching cmds
+
+const fetchCmd = (location) => {
+    fs.readdirSync(`${location}`).map((item) => {
+        if (!item.endsWith(".js") && !fs.statSync(path.join(location, item)).isDirectory()) {
+            return;
+        }
+
+        if (item.endsWith(".js")) { // possible command file
+            const command = require(path.join(location, item));
+            
+            if (!command.type || (!(command.name) && !(command.data)) || !command.execute) {
+                return console.warn(`Skipping ${location}/${item} as it is missing an attribute of 'type', 'name' or 'data', 'execute'`);   
+            }
+
+            switch (command.type) {
+                case 'prefix':
+                    return client.prefixCmds.set(command.name, command);
+                case 'slash':
+                    return client.slashCmds.set(command.name, command);
+                case 'sub':
+                    const full = `${location}${path.sep}${item}`;
+                    const commandsDir = path.join(__dirname, 'commands');
+                    const relative = path.relative(commandsDir, full);
+                    const withoutExtension = relative.slice(0, -path.extname(relative).length);
+                    const dotNotation = withoutExtension.split(path.sep).join('.');
+                    return client.subcommandHandlers.set(dotNotation, command);
+                default:
+                    return console.warn(`Skipping ${location}/${item} as it is has an unknown command type.`);
+            }
+        } else { // possible sub command files / grouped commands
+            return fetchCmd(`${location}${path.sep}${item}`);
+        }
+    });
+}
+const commandsPath = path.join(__dirname, "commands");
+fetchCmd(commandsPath);
+
+
+// command handling
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.slashCmds.get(interaction.commandName);
+
+    if (command) {
+        await command.execute();
+    } else {
+        const group = interaction.options.getSubcommandGroup(false);
+        const subcommand = interaction.options.getSubcommand(false);
+
+        let key;
+        if (group) {
+            key = `${interaction.commandName}.${group}`;
+        } else if (subcommand) {
+            key = `${interaction.commandName}.${subcommand}`;
+        }
+
+        if (!key) return await interaction.reply({ content: `Command not found.`, flags: MessageFlags.Ephemeral });
+
+        const handler = client.subcommandHandlers.get(key);
+        if (handler) {
+            await handler.execute(interaction);
+        } else {
+            return await interaction.reply({ content: `Command handler not found.`, flags: MessageFlags.Ephemeral });
+        }
+    }
+});
+
+
+client.on(Events.MessageCreate, async message => {
+    if (message.author.bot) return;
+    if (message.content[0] === ".") {
+        const args = message.content.slice(1).trim().split(/ +/);
+        const name = args.shift().toLowerCase();
+        const command = client.prefixCmds.get(name);
+
+        if (!command) return;
+
+        await command.execute(message, args);
+    }
+})
+
+
+
+
+// on ready
+client.once(Events.ClientReady, (client) => {
+    client.user.setStatus('dnd');
+    client.user.setActivity({ type: 3, name: 'Tactor Development'});
+});
+
+
+
+// bot start
+client.login(TOKEN);
