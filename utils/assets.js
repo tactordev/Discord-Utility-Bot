7b447692a6cdbe6ffce9e61a628c41c8eeb5758f
Tactor <119103845+tactordev@users.noreply.github.com>
2026-07-30 11:32:35 +0100

    Add emoji assets

---
+++ utils/assets.js
@@ -0,0 +1,13 @@
+
+const loading = `<a:tdloading:1372870759101239326>`;
+const success = `<:check:1365656295838978109>`;
+const error = `<:error:1365656796450000937>`;
+
+
+module.exports = {
+    emojis: {
+        loading: loading,
+        success: success,
+        error: error
+    }
+}
