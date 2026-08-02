76ae23b12cbb276c1db58e4b7ed3cc33e7c0c982
Tactor <119103845+tactordev@users.noreply.github.com>
2026-08-02 17:46:45 +0100

    [UTILS] More asset emojis

--- utils/assets.js
+++ utils/assets.js
@@ -2,12 +2,17 @@
 const loading = `<a:tdloading:1372870759101239326>`;
 const success = `<:check:1365656295838978109>`;
 const error = `<:error:1365656796450000937>`;
-
+const refresh = `<:refresh:1533502142072230071>`;
+const left = `<:left:1533502706105450669>`;
+const right = `<:right:1533502668230754326>`;
 
 module.exports = {
     emojis: {
         loading: loading,
         success: success,
-        error: error
+        error: error,
+        refresh: refresh,
+        left: left,
+        right: right
     }
 }
