e28329ded66f004229d582f6273a7ace8d212572
Tactor <me@tactor.dev>
2026-08-02 12:29:48 +0100

    [COMMAND]: Timeout command

---
+++ commands/moderation/timeout.js
@@ -0,0 +1,51 @@
+const discord = require('discord.js');
+const { emojis } = require('#utils/assets');
+const { fetchUser } = require('#utils/member');
+const { extractTime } = require('#utils/parsing');
+
+module.exports = {
+    name: 'timeout',
+    description: 'Timeout a user',
+    type: 'prefix',
+    permissions: [discord.PermissionsBitField.Flags.MuteMembers, discord.PermissionsBitField.Flags.ModerateMembers, discord.PermissionsBitField.Flags.Administrator],
+    async execute(client, message, initialResponse, args) {
+        await message.delete().catch(() => {});
+
+        const format = `timeout [user*] [duration*] [reason ? No reason provided.]`;
+
+        if (args.length < 2) return await initialResponse.edit({
+            content: `${emojis.error} Missing required arguments.\n-# ${format}`
+        });
+
+        const member = args[0];
+        const raw = args.slice(1);
+
+        const user = await fetchUser(member, message.guild);
+
+        if (!user) return await initialResponse.edit({
+            content: `${emojis,error} Invalid user provided: **${member}**.\n-# ${format}`
+        });
+
+        const data = extractTime(raw.join(' '));
+
+        let rawDuration;
+        let duration;
+        let reason;
+
+        if (data) {
+            ({
+                rawDuration,
+                duration,
+                reason
+            } = data);
+        }
+
+        if (!rawDuration) return await initialResponse.edit({
+            content: `${emojis.error} Invalid duration provided.\n-# ${format}`
+        });
+
+        return await initialResponse.edit({
+            content: `${emojis.success} Successfully timed out **${user.user.username}** for **${rawDuration}** for **${reason?.length > 0 ? reason : "no reason provided"}**.`
+        });
+    }
+}
