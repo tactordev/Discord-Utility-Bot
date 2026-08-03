56d4ab8ece1ddb9426f0b8cf9609384459484bcd
Tactor <119103845+tactordev@users.noreply.github.com>
2026-08-03 17:14:28 +0100

    [UTILITY] Info & cog icons

--- utils/assets.js
+++ utils/assets.js
@@ -2,7 +2,13 @@
 const loading = `<a:tdloading:1372870759101239326>`;
 const success = `<:check:1365656295838978109>`;
 const error = `<:error:1365656796450000937>`;
+
+
 const refresh = `<:refresh:1533502142072230071>`;
+const info = `<:info:1533768559283409057>`;
+const cog = `<:cog:1533768400860479608>`;
+
+
 const left = `<:left:1533502706105450669>`;
 const right = `<:right:1533502668230754326>`;
 
@@ -11,7 +17,11 @@ module.exports = {
         loading: loading,
         success: success,
         error: error,
+
         refresh: refresh,
+        info: info,
+        cog: cog,
+        
         left: left,
         right: right
     }
